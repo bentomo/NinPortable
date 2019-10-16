6e8fb350f7104f2f155023eb15a05da9a1eeb52d
Austin Carter <austinbennettcarter@gmail.com>
2019-10-16 14:58:20 -0500

    Adding self reset to top wrapper

--- hw/top/top_np.v
+++ hw/top/top_np.v
@@ -30,48 +30,69 @@ module top_np(
 	input wire CLK,
 	input wire RST,
 
+	
+
 	input wire SERIAL_RX,
 	output reg SERIAL_TX
 
 	);
 
+	wire core_clock;
+	assign core_clock <= CLK;
 
-picosoc cpu (
-	.clk (),
-	.resetn (),
+	//self reset
+	reg [7:0] reset_counter;
+	assign reset_core = ~reset_counter[7];
+	`define RESET_TIME 'b100
+	always @ (posedge core_clock) begin
+	    if (RST == 0) begin
+	        reset_counter <= 0;
+	    end else begin
+	        if (reset_counter == `RESET_TIME) begin
+	            //nothing to do, reset time reached
+	        end else begin
+	            reset_counter <= reset_counter + 1;
+	        end
+	    end
+	end
 
-	.iomem_valid (),
-	.iomem_ready (),
-	.iomem_wstrb (),
-	.iomem_addr (),
-	.iomem_wdata (),
-	.iomem_rdata (),
+	wire cpu_reset; assign cpu_reset ~= reset_core;
+	picosoc cpu (
+		.clk (core_clock),
+		.resetn (cpu_reset),
 
-	.irq_5 (),
-	.irq_6 (),
-	.irq_7 (),
+		.iomem_valid (),
+		.iomem_ready (),
+		.iomem_wstrb (),
+		.iomem_addr (),
+		.iomem_wdata (),
+		.iomem_rdata (),
 
-	.ser_tx ( SERIAL_TX ),
-	.ser_rx ( SERIAL_RX ),
+		.irq_5 (),
+		.irq_6 (),
+		.irq_7 (),
 
-	.flash_csb (),
-	.flash_clk (),
+		.ser_tx ( SERIAL_TX ),
+		.ser_rx ( SERIAL_RX ),
 
-	.flash_io0_oe (),
-	.flash_io1_oe (),
-	.flash_io2_oe (),
-	.flash_io3_oe (),
+		.flash_csb (),
+		.flash_clk (),
 
-	.flash_io0_do (),
-	.flash_io1_do (),
-	.flash_io2_do (),
-	.flash_io3_do (),
+		.flash_io0_oe (),
+		.flash_io1_oe (),
+		.flash_io2_oe (),
+		.flash_io3_oe (),
 
-	.flash_io0_di (),
-	.flash_io1_di (),
-	.flash_io2_di (),
-	.flash_io3_di ()
-);
+		.flash_io0_do (),
+		.flash_io1_do (),
+		.flash_io2_do (),
+		.flash_io3_do (),
+
+		.flash_io0_di (),
+		.flash_io1_di (),
+		.flash_io2_di (),
+		.flash_io3_di ()
+	);
 
 
 endmodule
